9ce81a66b3d35cee0109fe248abbee67ae8fe8f3
Jheison J. Mayta C <jmayta.caceres@gmail.com>
2022-09-09 12:08:51 -0500

    1.7, step2

--- exercises/part1/unicafe/src/index.js
+++ exercises/part1/unicafe/src/index.js
@@ -7,35 +7,40 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  const handleGood = () => {
+    setGood(good + 1);
+  };
+  const handleNeutral = () => {
+    setNeutral(neutral + 1);
+  };
+  const handleBad = () => {
+    setBad(bad + 1);
+  };
+
+  const allFeedbacks = () => good + neutral + bad;
+  const avgFeedback = () => {
+    const avg = (good * 1 + neutral * 0 + bad * -1) / allFeedbacks();
+    return isNaN(avg) ? 0 : avg.toFixed(2);
+  };
+  const positiveFeedbackRate = () => {
+    const rate = (100 * good) / allFeedbacks();
+    return isNaN(rate) ? 0 : rate.toFixed(2);
+  };
+
   return (
     <div>
       <h1>give feedback</h1>
-      <button
-        onClick={() => {
-          setGood(good + 1);
-        }}
-      >
-        good
-      </button>
-      <button
-        onClick={() => {
-          setNeutral(neutral + 1);
-        }}
-      >
-        neutral
-      </button>
-      <button
-        onClick={() => {
-          setBad(bad + 1);
-        }}
-      >
-        bad
-      </button>
+      <button onClick={handleGood}>good</button>
+      <button onClick={handleNeutral}>neutral</button>
+      <button onClick={handleBad}>bad</button>
 
       <h2>statistics</h2>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {allFeedbacks()}</p>
+      <p>average {avgFeedback()}</p>
+      <p>positive {positiveFeedbackRate()} %</p>
     </div>
   );
 };
