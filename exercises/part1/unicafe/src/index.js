e12e3e8c68582b44c5cbf280f9bb780fb725a943
Jheison J. Mayta C <jmayta.caceres@gmail.com>
2022-09-09 12:18:31 -0500

    1.8, step3

--- exercises/part1/unicafe/src/index.js
+++ exercises/part1/unicafe/src/index.js
@@ -1,6 +1,26 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 
+const Statistics = ({ good, neutral, bad }) => {
+  const allFeedbacks = () => good + neutral + bad;
+  const avgFeedback = () => {
+    const avg = (good * 1 + neutral * 0 + bad * -1) / allFeedbacks();
+    return isNaN(avg) ? 0 : avg.toFixed(2);
+  };
+  const positiveFeedbackRate = () => {
+    const rate = (100 * good) / allFeedbacks();
+    return isNaN(rate) ? 0 : rate.toFixed(2);
+  };
+
+  return (
+    <div className="statistics">
+      <p>all {allFeedbacks()}</p>
+      <p>average {avgFeedback()}</p>
+      <p>positive {positiveFeedbackRate()} %</p>
+    </div>
+  );
+};
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
@@ -17,16 +37,6 @@ const App = () => {
     setBad(bad + 1);
   };
 
-  const allFeedbacks = () => good + neutral + bad;
-  const avgFeedback = () => {
-    const avg = (good * 1 + neutral * 0 + bad * -1) / allFeedbacks();
-    return isNaN(avg) ? 0 : avg.toFixed(2);
-  };
-  const positiveFeedbackRate = () => {
-    const rate = (100 * good) / allFeedbacks();
-    return isNaN(rate) ? 0 : rate.toFixed(2);
-  };
-
   return (
     <div>
       <h1>give feedback</h1>
@@ -38,9 +48,8 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
-      <p>all {allFeedbacks()}</p>
-      <p>average {avgFeedback()}</p>
-      <p>positive {positiveFeedbackRate()} %</p>
+
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
