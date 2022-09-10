e4b59611ba8a0523cd298bb1e5d0d320d980a5f5
Jheison J. Mayta C <jmayta.caceres@gmail.com>
2022-09-10 18:53:47 -0500

    2.3 step8

--- exercises/part1/courseinfo/src/components/Content.jsx
+++ exercises/part1/courseinfo/src/components/Content.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Part from "./Part";
 
-export default function Content(props) {
+export default function Content({ parts }) {
+  const total = parts.reduce((acumulator, { exercises }) => {
+    return acumulator + exercises;
+  }, 0);
+
   return (
     <div>
-      {props.parts.map((part) => {
+      {parts.map((part) => {
         return (
           <Part key={part.id} part={part.name} exercises={part.exercises} />
         );
