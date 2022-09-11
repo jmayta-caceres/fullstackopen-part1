a048a3d13c732bf00d214767cd30afd0a38f95c7
Jheison J. Mayta C <jmayta.caceres@gmail.com>
2022-09-11 02:07:44 -0500

    2.9 step4

--- exercises/part2/phonebook/src/App.js
+++ exercises/part2/phonebook/src/App.js
@@ -1,11 +1,18 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([
+  const fakePersons = [
     { id: 1, name: "Arto Hellas", number: "934-6598-123" },
-  ]);
+    { id: 2, name: "Mr. Rickey Dach", number: "1-077-045-0320" },
+    { id: 3, name: "Serenity Abbott", number: "+03(1)9301810931" },
+    { id: 4, name: "Chaz Conn II", number: "724-248-5072" },
+    { id: 5, name: "Prof. Retta Renner II", number: "1-149-786-7867x617" },
+    { id: 6, name: "Hudson Yost", number: "(405)729-6382" },
+  ];
+  const [persons, setPersons] = useState(fakePersons);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [query, setQuery] = useState("");
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
@@ -13,6 +20,10 @@ const App = () => {
 
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value);
+  };
+
+  const handleSearch = (event) => {
+    setQuery(event.target.value);
   };
 
   const savePerson = (event) => {
@@ -35,9 +46,21 @@ const App = () => {
     setNewNumber("");
   };
 
+  const personsToShow =
+    query === ""
+      ? persons
+      : persons.filter((person) => {
+          return person.name.toLowerCase().includes(query.toLowerCase());
+        });
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with
+        <input value={query} onChange={handleSearch} type="text" />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={savePerson}>
         <div>
           name:
@@ -52,10 +75,10 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
-        <p key={person.id}>
-          {person.name} - {person.number}
-        </p>
+      {personsToShow.map((person) => (
+        <div key={person.id}>
+          {person.name} / <em>{person.number}</em>
+        </div>
       ))}
     </div>
   );
